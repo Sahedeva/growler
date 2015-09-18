437342071d4036f95677d46d77a35c5297e4ff4b
Robert Weilbaecher <robertweilbaecher@hotmail.com>
2015-09-18 12:39:31 -0500

    Growl input done

--- growler.rb
+++ growler.rb
@@ -1,11 +1,25 @@
-require 'sinatra'
-require 'sinatra/reloader'
+enable :sessions
 
-before do |variable|
-	
+before do
+	session[:roars] ||= []
+	session[:time_stamp] ||= []
 end
 
 # Routes
 get '/' do
 	erb :index
 end
+
+get '/add_growl' do 
+	# We use this view to display the form
+	erb :add_growl
+end
+
+post '/' do
+	erb :add_growl
+	if logic (length of growl <=142)
+	session[:roars] << params[:growl]
+	session[:time_stamp] << Time.now.strftime("%m/%d/%Y %H:%M %p")
+	else redirect to invalid page
+end
+
